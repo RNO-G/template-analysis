0631a5e4edb02ddf1255a10efc2e5b30dcff4de5
Steffen Hallmann <steffen.hallmann@desy.de>
2023-03-06 14:19:19 +0100

    add shebangs

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
 setup(name='template_analysis', # rename to the name of your analysis
--- template_analysis/config.py
+++ template_analysis/config.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 
 # some common directory paths
