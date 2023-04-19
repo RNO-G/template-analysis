3a1a962087d0d2fa38c00c34efca1ff5d5e2ba70
Steffen Hallmann <steffen.hallmann@desy.de>
2023-04-19 22:00:16 +0200

    autodetect

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='template_analysis', # rename to the name of your analysis
       version='0.1', # increase as needed while developing your code
@@ -9,6 +10,6 @@ setup(name='template_analysis', # rename to the name of your analysis
       author='The RNO-G Collaboration, Steffen Hallmann', # put your name
       author_email='steffen.hallmann@desy.de', # put your email adress
       license='MIT',
-      packages=['template_analysis'], # put the sub directory where your analysis sits
-      entry_points = {'console_scripts': ["run-template-analysis=template_analysis.run:main"]}, #edit and add more commands to the list if need be
+      packages=find_packages(), # alternatively, put the list of sub directories where your analyses sit
+      #entry_points = {'console_scripts': ["run-template-analysis=template_analysis.run:main"]}, #uncomment and add more commands to the list if need be
       zip_safe=False)
